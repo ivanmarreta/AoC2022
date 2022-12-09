6a3aad047edb7d5c322426bd29431d37d0c315a4
Ivan Oliveira <ioliveira@linkare.com>
2022-12-09 12:25:39 +0000

    Day 1 - part 1

---
+++ src/me/ivanmarreta/calendar/Day1.java
@@ -0,0 +1,34 @@
+package me.ivanmarreta.calendar;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Day1 {
+    public static void main(String[] args) throws IOException{
+
+        String fileName = "inputs/day1.txt";
+
+        List<Integer> totalCalories = new ArrayList<>();
+
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+            int caloriesByElf = 0;
+            for (String line : stream.toList()) {
+                if (line.isBlank()) {
+                    totalCalories.add(caloriesByElf);
+                    caloriesByElf = 0;
+                    continue;
+                }
+                caloriesByElf += Integer.parseInt(line.strip());
+            }
+        }
+        
+        Integer part1Result = Collections.max(totalCalories);
+        System.out.println(part1Result);
+    }
+
+}
