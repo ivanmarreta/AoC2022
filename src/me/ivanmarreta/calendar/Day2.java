9dab1ca04d20f55653ceea34c5bb8d8f9cae3a5a
Ivan Marrêta <ivanmarreta@gmail.com>
2022-12-10 01:15:55 +0000

    Day 2 - part 2

--- src/me/ivanmarreta/calendar/Day2.java
+++ src/me/ivanmarreta/calendar/Day2.java
@@ -10,42 +10,43 @@ import java.util.stream.Stream;
 public class Day2 {
 
     public static void main(String[] args) throws IOException {
-        String fileName = "inputs/day2.txt";
 
-        String inputs = """
-                A Y
-                B X
-                C Z
-                """;
-
-        final int ROCK = 1;
-        final int PAPER = 2;
+        final int ROCK     = 1;
+        final int PAPER    = 2;
         final int SCISSORS = 3;
 
         final int LOSS = 0;
         final int DRAW = 3;
-        final int WIN = 6;
+        final int WIN  = 6;
 
-        Map<String, Integer> strategy = new HashMap<>();
-        strategy.put("A X", ROCK + DRAW);
-        strategy.put("A Y", PAPER + WIN);
-        strategy.put("A Z", SCISSORS + LOSS);
-        strategy.put("B X", ROCK + LOSS);
-        strategy.put("B Y", PAPER + DRAW);
-        strategy.put("B Z", SCISSORS + WIN);
-        strategy.put("C X", ROCK + WIN);
-        strategy.put("C Y", PAPER + LOSS);
-        strategy.put("C Z", SCISSORS + DRAW);
+        Map<String, Integer[]> strategy = new HashMap<>();
+        strategy.put("A X", new Integer[] {ROCK + DRAW, 3});
+        strategy.put("A Y", new Integer[] {PAPER + WIN, 4});
+        strategy.put("A Z", new Integer[] {SCISSORS + LOSS, 8});
+        strategy.put("B X", new Integer[] {ROCK + LOSS, 1});
+        strategy.put("B Y", new Integer[] {PAPER + DRAW, 5});
+        strategy.put("B Z", new Integer[] {SCISSORS + WIN, 9});
+        strategy.put("C X", new Integer[] {ROCK + WIN, 2});
+        strategy.put("C Y", new Integer[] {PAPER + LOSS, 6});
+        strategy.put("C Z", new Integer[] {SCISSORS + DRAW, 7});
 
+        String fileName = "inputs/day2.txt";
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 
-            int score = 0;
+            int part1 = 0;
+            int part2 = 0;
+
             for (String line : stream.toList()) {
-                score += strategy.get(line.strip());
+                Integer[] scores = strategy.get(line.strip());
+                part1 += scores[0];
+                part2 += scores[1];
             }
 
             //PART 1
-            System.out.println(score);
+            System.out.println(part1);
+
+            //PART 2
+            System.out.println(part2);
         }
     }
 }
